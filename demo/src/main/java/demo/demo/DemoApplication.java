25c54298f97cb7c05dcd24dbe3f07aed6533a0c4
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-22 22:52:10 +0530

    Update

--- demo/src/main/java/demo/demo/DemoApplication.java
+++ demo/src/main/java/demo/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
+	@GetMapping("/")
+	public String rootEndpoint() {
+		String message = "Hello, world!";
+		return message;
+	}
 }
